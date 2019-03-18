ea2c72afb8a77e6c8c16f80ad62ca95652a518d5
Юрий <yurialekseev@mail.ru>
2019-03-18 18:31:44 +0300

    all done

--- lesson-1/AlekseevYuriy/homework.js
+++ lesson-1/AlekseevYuriy/homework.js
@@ -3,32 +3,31 @@
   Возвращает true, если все аргументы, кроме первого входят в первый.
   Первым всегда должен быть массив.
 */
-
-function isInArray<T>(array: Array<T>, ...args: T[]):boolean {
- 
-    return args.every(v=>array.indexOf(v) > -1);
-    
+function isInArray(array) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
     }
-
-let result1 = isInArray<number>([2,3,14,5],2,14,1);
+    return args.every(function (v) { return array.indexOf(v) > -1; });
+}
+var result1 = isInArray([2, 3, 14, 5], 2, 14, 1);
 console.log(result1);
 /*
 2)
  Написать функцию summator(), которая суммирует переданые ей аргументы.
  Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
 */
-
-function summator(...args: (number | string)[]):number | string{
- 
-  var res = args.reduce(function(prev, curr): number{
+function summator() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    var res = args.reduce(function (prev, curr) {
         return (Number(prev) || 0) + (Number(curr) || 0);
     });
-
-return res;
-
-}   
-
-let result2 = summator(2,'50',300);
+    return res;
+}
+var result2 = summator(2, '50', 300);
 console.log(result2);
 /*
 3)
@@ -37,22 +36,20 @@ console.log(result2);
   Порядок элементов результирующего массива должен совпадать с порядком,
   в котором они встречаются в оригинальной структуре.
 */
-
-function getUnique(...args: number[]):number[]{
- 
-    let resultArray: number[] = [];
-
-    args.forEach(function(item) {
-      if(!isInArray<number>(resultArray,item))
-        resultArray.push(item);
+function getUnique() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    var resultArray = [];
+    args.forEach(function (item) {
+        if (!isInArray(resultArray, item))
+            resultArray.push(item);
     });
-
     return resultArray;
-}   
-
-let result3 = getUnique(5,5,12,3,1,6,6,7,2,2);
+}
+var result3 = getUnique(5, 5, 12, 3, 1, 6, 6, 7, 2, 2);
 console.log(result3);
-
 /*
 4)
  Дописать функцию toMatrix(data, rowSize), которая принимает аргументом массив и число,
@@ -60,27 +57,24 @@ console.log(result3);
  элементы подмассивов беруться из массива data.
  Оригинальный массив не должен быть изменен.
 */
+function toMatrix(data, rowSize) {
+    var resultArray = [[]];
+    var currRowSize = 0;
+    var currRow = 0;
 
-function toMatrix(data: number[], rowSize: number) : number[][] {
-  var resultArray: number[][] = [[]];
-  var currRowSize = 0;
-  var currRow = 0;
+    data.forEach(function (item) {
+        if (!resultArray[currRow])
+            resultArray[currRow] = new Array(rowSize);
 
-  data.forEach(function (item) {
-      
-      if (!resultArray[currRow])
-          resultArray[currRow] = new Array(rowSize);
-      
-      resultArray[currRow].push(item);
-      currRowSize++;
-      
-      if(currRowSize > rowSize - 1) {
-          currRowSize = 0;
-          currRow++;
-      }
-  });
-  return resultArray;
+        resultArray[currRow].push(item);
+        currRowSize++;
+        
+        if (currRowSize > rowSize - 1) {
+            currRowSize = 0;
+            currRow++;
+        }
+    });
+    return resultArray;
 }
-
-let result4 = toMatrix([2,3,5,6,7,7,8,8,9,9,9,9,12,11,-1,13,5],4);
+var result4 = toMatrix([2, 3, 5, 6, 7, 7, 8, 8, 9, 9, 9, 9, 12, 11, -1, 13, 5], 4);
 console.log(result4);
--- src/example.js
+++ src/example.js
@@ -1,35 +1,71 @@
 "use strict";
-Object.defineProperty(window, "MySweetApp", { value: "v1.0.0", writable: true });
-function deliveryMethod() {
-    // TODO
-    return "overnight";
+// Object.defineProperty(window, 'MySweetApp', { value: 'v1.0.0', writable: true });
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var xObj = {
+    x: 10,
+};
+xObj.x = 2;
+var SuperPoint = /** @class */ (function () {
+    function SuperPoint() {
+        this.y = 10;
+    }
+    SuperPoint.prototype.chartName = function () {
+        return 'My Chart';
+    };
+    return SuperPoint;
+}());
+var Point = /** @class */ (function (_super) {
+    __extends(Point, _super);
+    function Point() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Point.prototype.sum = function () {
+        return this.y * 10;
+    };
+    return Point;
+}(SuperPoint));
+var CustomPoint = /** @class */ (function (_super) {
+    __extends(CustomPoint, _super);
+    function CustomPoint() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    CustomPoint.prototype.sum = function () {
+        return this.y * 2;
+    };
+    return CustomPoint;
+}(SuperPoint));
+var point = new Point(1, 2, 3);
+// class CustomPoint extends Point {
+//     public constructor() {
+//         super();
+//     }
+// }
+// class Singleton {
+//     private static _instance: Singleton;
+//     private constructor() {
+//     }
+//     public static getInstance(): Singleton {
+//         if (!Singleton._instance) {
+//             Singleton._instance = new Singleton();
+//         }
+//         return Singleton._instance;
+//     }
+// }
+// let instance = Singleton.getInstance();
+// type, class;
+function a(_a) {
 }
-function shipWeight() {
-    var el = document.getElementById('weight');
-    if (!el) {
-        return 0;
-    }
-    return parseInt(el.innerHTML);
-}
-/**
-* @param {(string | string[])} emailAddr - An email address of array of email addresses
-*/
-function sendUpdates(emailAddr) {
-    function sendEmail(addr) {
-        // Default to standard delivery if empty
-        console.log("Shipping to " + addr + " via " + (deliveryMethod() || "standard") + " delivery");
-        if (shipWeight() > 100) {
-            console.log("WARNING: Oversize package");
-        }
-    }
-    // If it's an array, loop over it
-    if (Array.isArray(emailAddr)) {
-        emailAddr.forEach(function (val) {
-            sendEmail(val.trim());
-        });
-    }
-    else {
-        sendEmail(emailAddr.trim());
-    }
-}
-sendUpdates('asdasd');
+var user;
+var user1;
