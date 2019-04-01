7e03d829a1c77e9c6bd08a21e5e20277e2d7be08
roman kovalskiy <rkovalskyi@deloitte.com>
2019-04-01 10:20:44 +0300

    homework

--- src/rxjs.ts
+++ src/rxjs.ts
@@ -1,342 +1,120 @@
-// ReactiveX = iterator + observer
-// const sequence$: Observable<string | number> = of(...[1, 2, 3, 4]);
+import { fromEvent, Observable, of } from 'rxjs';
+import { debounceTime, delay, filter, map, switchMap, tap } from 'rxjs/operators';
 
-// sequence$
-//     .subscribe((value: string | number) => {
-//         // tslint:disable-next-line:no-console
-//         console.log(value);
-//     }, () => { }, () => {
-//         // tslint:disable-next-line:no-console
-//         console.log('complete');
-//     });
-// let count: number = 0;
-
-
-// const sequence$: Observable<number> = new Observable((observer: Observer<number>) => {
-//     setInterval(() => {
-//         count++;
-//         observer.next(count);
-//     }, 1000);
-// });
-
-
-// sequence$
-//     .subscribe((value: string | number) => {
-//         // tslint:disable-next-line:no-console
-//         console.log(`Sub 1  ---> ${value}`);
-//     }, () => { }, () => {
-//         // tslint:disable-next-line:no-console
-//         console.log('complete');
-//     });
-
-// setTimeout(() => {
-//     sequence$
-//         .subscribe((value: string | number) => {
-//             // tslint:disable-next-line:no-console
-//             console.log(`Sub 2  ---> ${value}`);
-//         }, () => { }, () => {
-//             // tslint:disable-next-line:no-console
-//             console.log('complete');
-//         });
-// }, 5000);
-
-// const sequence$: Observable<MouseEvent> = fromEvent(document, 'click') as Observable<MouseEvent>;
-
-// sequence$
-//     .subscribe((value: MouseEvent) => {
-//         // tslint:disable-next-line:no-console
-//         console.log(`Sub 1  ---> ${value.clientX}`);
-//     }, () => { }, () => {
-//         // tslint:disable-next-line:no-console
-//         console.log('complete');
-//     });
-
-// setTimeout(() => {
-//     sequence$
-//         .subscribe((value: MouseEvent) => {
-//             // tslint:disable-next-line:no-console
-//             console.log(`Sub 2  ---> ${value.clientX}`);
-//         }, () => { }, () => {
-//             // tslint:disable-next-line:no-console
-//             console.log('complete');
-//         });
-// }, 5000);
-
-// sequence$.next(21231);
+type resultItemType = { link: string, title: string };
+const stabResults = [
+    {
+        link: 'https://stackblitz.com/edit/angular-gcv2ip?file=src%2Fapp%2Fapp.component.ts',
+        title: 'Demo Angular 6 + Rxjs 6'
+    },
+    {
+        link: 'https://angular.io/guide/http#error-handling',
+        title: 'Angular Error handling'
+    },
+    {
+        link: 'https://www.learnrxjs.io/operators/utility/delay.html',
+        title: 'Delay for increasing durations'
+    },
+    {
+        link: 'https://vuejs.org/',
+        title: 'vue js'
+    },
+    {
+        link: 'https://github.com/vuejs/vue',
+        title: 'GitHub - vuejs/vue: Vue.js is a progressive, incrementally-adoptable'
+    },
+    {
+        link: 'https://habr.com/ru/post/329452/',
+        title: 'Vue.js для сомневающихся. Все, что нужно знать / Хабр - Habr'
+    },
+    {
+        link: 'https://reactjs.org/',
+        title: 'React – A JavaScript library for building user interfaces'
+    },
+    {
+        link: 'React Native · A framework for building native apps using React',
+        title: 'https://facebook.github.io/react-native/'
+    }
+];
 
 
-// const sequence1$: Observable<number> = interval(1000);
+function getInput() {
+    return document.getElementById( 'search' );
+}
 
-/*
-sequence1$:  ---0---1---2---3---4--
-                 map((x)=>x*2)
-sequence2$:  ---0---2---4---6---8--
-*/
+function getEvent$( element: HTMLElement | null ): Observable<KeyboardEvent> {
+    const targetEl = element ? element : document;
 
-// const sequence2$: Observable<number> = sequence1$
-//     .pipe(
-//         map((x: number) => x * 2),
-//     );
+    return fromEvent<KeyboardEvent>( targetEl, 'keyup' ) // TODO: if user delete some symbols ?
+        .pipe(
+            debounceTime( 250 )
+        );
+}
 
-// sequence2$.subscribe((value: number) => {
-//     // tslint:disable-next-line:no-console
-//     console.log(value);
-// });
+function mapEventToStringValue( event$: Observable<KeyboardEvent> ) {
+    return event$
+        .pipe(
+            map( ( e: KeyboardEvent ) => {
+                if (e && e.target) {
+                    return (e.target as HTMLInputElement).value;
+                }
 
+                return '';
+            } ),
+            filter( ( res: string ) => res.length >= 1 )
+        );
+}
 
-// const sequence1$: Observable<number> = interval(1000);
-
-// /*
-// sequence1$:  ---0---1---2---3---4--
-//                  tap((x)=>{console.log(x); return x*2})
-//              ---0---1---2---3---4--
-//                  map((x)=>x*2)
-// sequence2$:  ---0---2---4---6---8--
-// */
-// const sequence2$: Observable<number> = sequence1$
-//     .pipe(
-//         tap((x: number) => {
-//             // tslint:disable-next-line:no-console
-//             console.log('From console', x);
-//             return x * 2;
-//         }),
-//         map((x: number) =>  x * 2),
-//     );
-
-// sequence2$.subscribe((value: number) => {
-//     // tslint:disable-next-line:no-console
-//     console.log(value);
-// });
-
-// const sequence1$: Observable<number> = interval(1000);
-
-// /*
-// sequence1$:  ---0---1---2---3---4--
-//                  take(3)
-//              ---0---1---2|
-//                  skip(2)
-// sequence2$:  -----------2
-// */
-// const sequence2$: Observable<number> = sequence1$
-//     .pipe(
-//         take(3),
-//         skip(2)
-//     );
-
-// sequence2$.subscribe((value: number) => {
-//     // tslint:disable-next-line:no-console
-//     console.log(value);
-// }, () => { }, () => {
-//     // tslint:disable-next-line:no-console
-//     console.log('complete');
-// });
-
-
-// const sequence1$: Observable<number> = interval(1000).pipe(take(4));
-// const sequence2$: Observable<number> = interval(1000).pipe(skip(6), take(4));
-// /*
-// sequence1$:  ---0---1---2---3|
-// sequence1$:  ----------------------------6---7---8--9|
-//                  concat
-// sequence2$:  ---0---1---2---3-------------------------6---7---8--9|
-// */
-// concat(sequence1$, sequence2$)
-//     .subscribe((value: number) => {
-//         // tslint:disable-next-line:no-console
-//         console.log(value);
-//     }, () => { }, () => {
-//         // tslint:disable-next-line:no-console
-//         console.log('complete');
-//     });
-
-
-// const sequence1$: Observable<number> = interval(500).pipe(take(4));
-// const sequence2$: Observable<number> = interval(300).pipe(take(5));
-/*
-sequence1$:  ----0----1----2----3|
-sequence1$:  --0--1--2--3--4|
-                 combineLatest()
-sequence2$:  ----[0,0][0,1]--[0,2]-[1,2]-[1,3]--[2,3][2,4]----[3,4]|
-*/
-// combineLatest(sequence1$, sequence2$)
-//     .subscribe((value: number[]) => {
-//         // tslint:disable-next-line:no-console
-//         console.log(value);
-//     }, () => { }, () => {
-//         // tslint:disable-next-line:no-console
-//         console.log('complete');
-//     });
-
-// const touchStart$: Observable<number> = getX(fromEvent(document, 'touchstart') as Observable<TouchEvent>);
-// const touchEnd$: Observable<number> = getX(fromEvent(document, 'touchend') as Observable<TouchEvent>);
-// const swipe$: Observable<number> = swipe(zip(touchStart$, touchEnd$));
-// swipe$.subscribe((direction: number) => {
-//     if (direction > 0) {
-//         // tslint:disable-next-line:no-console
-//         console.log('Swipe left');
-//         return;
-//     }
-//     // tslint:disable-next-line:no-console
-//     console.log('Swipe right');
-// });
-//
-// function getX(source$: Observable<TouchEvent>): Observable<number> {
-//     return source$
-//         .pipe(
-//             map(({ changedTouches }: TouchEvent) => changedTouches[0].clientX)
-//         );
-// }
-//
-// function swipe(source$: Observable<[number, number]>): Observable<number> {
-//     return source$.pipe(
-//         map(([startX, endX]: [number, number]) => {
-//             return startX - endX;
-//         })
-//     );
-// }
-
-// Subject => Observable + observer
-// sequence2$.next()
-
-// TODO custom operator example
-
-// class SkipLimitSubscriber extends Subscriber<number> {
-//
-//     private _count: number = 1;
-//     private _interval: number = 1;
-//
-//     public constructor(
-//         subscriber: Subscriber<number>,
-//         private _skip: number,
-//         private _limit: number
-//     ) {
-//         super(subscriber);
-//     }
-//
-//     protected _next(value: number): void {
-//         const borderLow: number = this._interval * (this._skip + this._limit) - this._limit;
-//         const borderHight: number = borderLow + this._limit;
-//         if (borderLow < this._count && this._count <= borderHight) {
-//             this.destination && this.destination.next && this.destination.next(value);
-//             this._count++;
-//             if (borderHight < this._count) {
-//                 this._interval++;
-//             }
-//             return;
-//         }
-//         this._count++;
-//     }
-//
-// }
-//
-// function skipLimit(skip: number, limit: number) {
-//     return (source: Observable<number>): Observable<number> => {
-//         return source.lift({
-//             call(subscriber: Subscriber<number>): void {
-//                 source.subscribe(new SkipLimitSubscriber(subscriber, skip, limit));
-//             }
-//         });
-//     };
-// }
-//
-// const sequence$: Observable<number> = interval(1000)
-//     .pipe(skipLimit(3, 4));
-//
-//
-// sequence$.subscribe((value: number) => {
-//         console.log(value);
-//     });
-
-
-// const sequence$: Observable<number> = interval(1000)
-//     .pipe(take(4));
-//
-// const hightOrderSequence$ = sequence$.pipe(
-//     switchMap((_value: number) => of(1, 2)),
-// );
-
-// getTableData$
-//     .pipe(switchMap(()=>{
-//         return checkToken$.
-//             pipe(switchMap(()=> post$))
-//     })
-
-// hightOrderSequence$.subscribe((value) => {
-//     console.log(value);
-// });
-
-// const clickSequence$ = fromEvent(document, 'click');
-// //
-// // function performedRequst() {
-// //     return fetch('https://jsonplaceholder.typicode.com/users/1')
-// //         .then(res => res.json());
-// // }
-// //
-// // clickSequence$
-// //     .pipe(mergeMap(_click => performedRequst()))
-// //     .subscribe(user => {
-// //         console.log(user.email);
-// //     });
-
-// Subject = Observable + observer
-
-
-// const sequence$$ = new ReplaySubject();
-// sequence$$.next(1);
-// sequence$$.next(2);
-// sequence$$.next(3);
-// sequence$$.next(4);
-// sequence$$.next(5);
-// sequence$$.next(6);
-// sequence$$.next('Angular');
-//
-// setTimeout(() => {
-//     sequence$$.next('Angular !!!!');
-// }, 5000);
-// setTimeout(() => {
-//     sequence$$
-//         .subscribe(value => console.log('Sub2', value));
-// }, 7000);
-import { interval, Observable } from 'rxjs';
-import { of } from 'rxjs/internal/observable/of';
-import { zip } from 'rxjs/internal/observable/zip';
-import { catchError, map, switchMap } from 'rxjs/operators';
-import { empty } from 'rxjs/internal/observable/empty';
-
-const sequence1$: Observable<number> = interval(500);
-const sequence2$: Observable<number | string> = of('1', '2', '3', 4, '5', '6',
-    '7'
-);
-
-zip(sequence1$, sequence2$)
-    .pipe(
-        switchMap(([_x, y]: [number, string | number]) => {
-            return of(null)
-                .pipe(
-                    map(() => {
-                        return (y as any).toUpperCase();
-                    }),
-                    // retryWhen(errObs => errObs.pipe(delay(3000)))
-                    catchError((err, _obs) => {
-                        console.log('Error 1', err);
-                        return empty();
-                    })
-                );
-        })
-    )
-    .subscribe(
-        (value) => {
-            console.log(value);
-        },
-        // null,
-        (err) => {
-            console.log('Error 2', err);
-        },
-        () => {
-            console.log('complete');
-        }
+function fetchResult( searchText: string ): Observable<resultItemType[]> {
+    return of( stabResults ).pipe(
+        map( results =>
+            results.filter( item =>
+                item.title.toLowerCase().indexOf( searchText ) !== -1
+            )
+        ),
+        delay( 1000 )
     );
+}
 
+function renderSearchResults( searchResults: resultItemType[] ): void {
+    const container = document.getElementById( 'resultsContainer' );
 
+    if (container) {
 
-// TODO AsyncSubject example
+        if (searchResults.length) {
+            container.innerHTML = '';
+            searchResults.forEach( item => {
+                const newLink = createLink( item );
+                container.appendChild( newLink );
+            } );
+            return;
+        }
+
+        container.innerHTML = '<div style="color: red">Nothing found</div>';
+        return;
+    }
+
+}
+
+function createLink( item: resultItemType ) {
+    const newLink = document.createElement( 'a' );
+    newLink.setAttribute( 'href', item.link );
+    newLink.setAttribute( 'style', 'display: block' );
+    const newText = document.createTextNode( item.title );
+    newLink.appendChild( newText );
+    return newLink;
+}
+
+const targetInput = getInput();
+const keyboardEvent$ = getEvent$( targetInput );
+const stringStream$ = mapEventToStringValue( keyboardEvent$ );
+
+stringStream$
+    .pipe(
+        switchMap( searchText => fetchResult( searchText )
+            .pipe(
+                tap( results => renderSearchResults( results ) )
+            )
+        )
+    )
+    .subscribe();
